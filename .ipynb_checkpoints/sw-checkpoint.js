f5a33672aac6124c6806e335adb0fdcbc2b5aad1
yoonsen <yoonsen@gmail.com>
2025-04-22 18:51:06 +0200

    PWA ready

---
+++ .ipynb_checkpoints/sw-checkpoint.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "spraaktrender-v1.0.0";
+const urlsToCache = [
+  "./",
+  "index.html",
+  "manifest.json",
+  "https://cdn.plot.ly/plotly-2.30.0.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+  "https://cdn-icons-png.flaticon.com/512/3898/3898157.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
